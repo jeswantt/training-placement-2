c4ba2391eba80eabcd362796b6eee308cb892457
jeswantt <jeswantt153@gmail.com>
2025-12-23 19:16:52 +0530

    Create 8.max consecutive ones.java

---
+++ 23.12.2025/8.max consecutive ones.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int n = nums.length;
+        int maxCount = 0;
+        
+        for (int i = 0; i < n; i++) {
+            int currentCount = 0;
+            for (int j = i; j < n; j++) {
+                if (nums[j] == 1) {
+                    currentCount++;
+                    maxCount = Math.max(maxCount, currentCount);
+                } else {
+                    break;
+                }
+            }
+        }
+        
+        return maxCount;
+    }
+}

3a3cc6d9a5d818f86caa7ef47b99401c24bf08bc
jeswantt <jeswantt153@gmail.com>
2026-01-18 11:58:05 +0530

    Create Rotate Image.py

---
+++ 16.01.2026/Rotate Image.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rotate(self, A):
+        n = len(A)
+        for i in range(n):
+            for j in range(i):
+                A[i][j], A[j][i] = A[j][i], A[i][j]
+        for row in A:
+            for j in range(n/2):
+                row[j], row[~j] = row[~j], row[j]
